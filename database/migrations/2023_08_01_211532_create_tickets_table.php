cd705ac5ee10b65c86231ae7c6f45934ef9e71a5
dimcodeur <dimcodeur@gmail.com>
2023-08-07 16:23:04 +0000

    version design1

--- database/migrations/2023_08_01_211532_create_tickets_table.php
+++ database/migrations/2023_08_01_211532_create_tickets_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
 
             $table->unsignedBigInteger('event_id');
             $table->unsignedBigInteger('user_id');
- 
+
             $table->foreign('user_id')->references('id')->on('users')->onDelete("cascade")->onUpdate("cascade");
             $table->foreign('event_id')->references('id')->on('events')->onDelete("cascade")->onUpdate("cascade");
             $table->timestamps();
