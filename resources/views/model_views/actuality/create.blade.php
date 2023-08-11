0823baef9db2899f39abd0a560c89ce7fd452d5d
dimcodeur <dimcodeur@gmail.com>
2023-08-11 13:18:09 +0000

    ajout des controllers

---
+++ resources/views/model_views/actuality/create.blade.php
@@ -0,0 +1,47 @@
+<x-app-layouts>
+    <div class="creation">
+        <!-- --------------------bouton listes des utilisateur -------------------------------- -->
+        <div class="listes">
+            <a href="#">Listes des utilisateurs</a>
+        </div>
+
+        <hr class="bars">
+        <!-- -------------------------formulaires ---------------------------------------------- -->
+        <form action="">
+            <div class="groupe">
+                <label for="">Nom : </label>
+                <input type="text" name="">
+            </div>
+
+            <div class="groupe">
+                <label for="">Email : </label>
+                <input type="email" name="">
+            </div>
+
+            <div class="groupe">
+                <label for="">Phone_number : </label>
+                <input type="text" name="">
+            </div>
+
+            <div class="groupe">
+                <label for="">Password : </label>
+                <input type="password" name="">
+            </div>
+
+            <div class="groupe">
+                <label for="">COnfirm_password : </label>
+                <input type="password" name="">
+            </div>
+
+            <div class="groupe">
+                <label for="">Role : </label>
+                <select name="" id="">
+                    <option value="">administrateur</option>
+                </select>
+            </div>
+            <div class="groupe">
+                <button type="submit">Valider</button>
+            </div>
+        </form>
+    </div>
+</x-app-layouts>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,18 @@ use App\Models\Payement;
 use App\Models\Reclamation;
 use App\Models\Score;
 use App\Models\Ticket;
+
+use App\Http\Controllers\ActualityController;
+use App\Http\Controllers\ActualityCommentController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\MatcheController;
+use App\Http\Controllers\OtherController;
+use App\Http\Controllers\PayementController;
+use App\Http\Controllers\ReclamationController;
+use App\Http\Controllers\ScoreController;
+use App\Http\Controllers\TicketController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,14 +50,14 @@ Route::middleware([
 });
 
 Route::resources([
-    "actuality"=>Actuality::class,
-    "actuality"=>ActualityComment::class,
-    "comment"=>Comment::class,
-    "event"=>Event::class,
-    "matche"=>Matche::class,
-    "other"=>Other::class,
-    "payement"=>Payement::class,
-    "reclamation"=>Reclamation::class,
-    "score"=>Score::class,
-    "ticket"=>Ticket::class,
+    // "actuality"=>Actuality::class,
+    // "actuality"=>ActualityComment::class,
+    // "comment"=>Comment::class,
+    "event"=>EventController::class,
+    // "matche"=>Matche::class,
+    // "other"=>Other::class,
+    // "payement"=>Payement::class,
+    // "reclamation"=>Reclamation::class,
+    // "score"=>Score::class,
+    // "ticket"=>Ticket::class,
 ]);
