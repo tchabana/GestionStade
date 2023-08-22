eb0f35876e5c19a791b18d2b598f0f5d91cc4418
ditoma95 <devgtfod@gmail.com>
2023-08-22 11:54:41 +0000

    modification des composant

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
         </button>
 
         <aside id="sidebar-multi-level-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-            <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-orange-800">
+            <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-yellow-700">
                 <ul class="space-y-2 font-medium">
                     <li>
                         <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
