eeb20b0c9853127c649603af4adfe7149f94db10
fayssol <fayssol.ogbone@gmail.com>
2023-08-29 12:57:33 +0000

    nbre de ticket en local

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -43,6 +43,10 @@ class PdfController extends Controller
                 'event_id' => $event->id,
                 'price' => $request->input('price'),
             ]);
+             // Mettre à jour le compteur dans l'événement associé
+            $event->nbr_ticket_gen_local = $event->nbr_ticket_gen_local + 1;
+            $event->save();
+
 
             $chemin = public_path('/codesQR/image_'.$i.'.svg');
             //ajout des data de ticket
