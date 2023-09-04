3f639a76e666e24f805c95cd8585abdf6a9a33da
ditoma95 <devgtfod@gmail.com>
2023-09-04 15:03:00 +0000

    modif tableau

--- resources/views/actions_users/contact.blade.php
+++ resources/views/actions_users/contact.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,7 +10,9 @@
         'resources/js/app.js',
     ])
 </head>
-<body>
+<body> --}}
+@extends('layouts.welcome')
+@section('appli')
     <section class="text-gray-600 body-font relative">
         <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
         <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
@@ -60,5 +62,6 @@
         </div>
         </div>
     </section>
-</body>
-</html>
+@endsection
+{{-- </body>
+</html> --}}
