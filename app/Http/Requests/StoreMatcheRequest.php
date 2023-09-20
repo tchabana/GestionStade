2ccaf11fa63b36d823369f0f6085ea86a098d3d7
tchabana <hafiztchabana2004@gmail.com>
2023-09-20 16:03:14 +0000

    b

--- app/Http/Requests/StoreMatcheRequest.php
+++ app/Http/Requests/StoreMatcheRequest.php
@@ -24,14 +24,24 @@ class StoreMatcheRequest extends FormRequest
      */
     public function rules(): array
     {
+        // Récupérez la date et l'heure actuelles
+        $now = now();
+        // Récupérez les valeurs de date et d'heure du formulaire
+        $date_start = $this->input('date_start');
+        $date_end = $this->input('date_end');
+        $start_at = $this->input('start_at');
+        $end_at = $this->input('end_at');
+        // Combinez la date et l'heure en une seule valeur DateTime
+        // $date_start_start_at = \Carbon\Carbon::parse("$date_start $start_at");
+        // $date_end_end_at = \Carbon\Carbon::parse("$date_end $end_at");
         return [
             'image_path'=>'required',
             'title'=>'required',
             'description'=>'required',
-            'date_start'=>'required',
-            'date_end'=>'required',
-            'start_at'=>'required',
-            'end_at'=>'required',
+            'date_start'=>['required','date','after_or_equal:'.$now],
+            'date_end'=>['required','date','after:'.$date_start],
+            'start_at'=>'required|timezone',
+            'end_at'=>'required|timezone|after:'.$start_at,
             'nbr_participant'=>'required',
             'authors'=>'required',
             'equipe1_name'=>'required',
