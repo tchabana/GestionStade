0aa7dc5a28970301f3b59ebb9625c153a3df6060
ditoma95 <devgtfod@gmail.com>
2023-09-20 13:40:17 +0000

    bbbbb

--- config/jetstream.php
+++ config/jetstream.php
@@ -50,7 +50,6 @@ return [
     |--------------------------------------------------------------------------
     | Features
     |--------------------------------------------------------------------------
-    |  b
     | Some of Jetstream's features are optional. You may disable the features
     | by removing them from this array. You're free to only remove some of
     | these features or you can even remove all of these if you need to.
