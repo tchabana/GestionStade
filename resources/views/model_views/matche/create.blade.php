43d2c2826938c2b590e707ba9efbfd3171507913
ditoma95 <devgtfod@gmail.com>
2023-09-21 17:13:10 +0000

    ajouter

--- resources/views/model_views/matche/create.blade.php
+++ resources/views/model_views/matche/create.blade.php
@@ -80,6 +80,30 @@
                         </div>
                     </div>
 
+{{-- ------------------------------------------------------------------------------------------------------------- --}}
+
+                    <div class="accord mt-8">
+                        <div class="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto    lg:px-8 sm:space-x-20 sm:space-y-0 space-y-4 sm:px-0 items-end">
+                            <div class="relative flex w-full">
+                                <div class="flex">
+                                    <input type="text" id="place"  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-transparent focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="Type de place">
+                                    <input type="number" id="prix"  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-transparent focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="Prix associé">
+                                </div>
+                                <button id="valider" class="w-10 h-10 bg-red-900 rounded-full text-white text-5xl px-1 cursor-pointer">+</button>
+                            </div>
+                            <div class="relative flex-grow w-full">
+                                <div id="allterrains">
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+
+{{-- -------------------------------------------------------------------------------------------------------------------------- --}}
                     <div class="flex lg:w-2/3  sm:flex-row flex-col mx-auto mt-8  lg:px-8 sm:space-x-20 sm:space-y-0 space-y-4 sm:px-0 items-center">
                         <div class="relative flex-grow w-full">
                         <input value="{{ old('image_path') }}" class="w-52" type="file" name="image_path" onchange="afficheImage(event)"  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-transparent focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" accept=".png, .jpg, .jpeg, .gif, .svg">
@@ -127,6 +151,107 @@
                 output.src = dataUrl;
             };
             reader.readAsDataURL(entrer.files[0]);
+        }
+//---------------------------------------------------------------------------------------------------------------------------
+        const terrains = [{id: 1, place: 'Vip',  prix: 500 }];
+        const valideButton = document.getElementById('valider');
+
+
+        valideButton.addEventListener('click', ajoutePlacePrix);
+
+        afficheTout();
+        updateDonneAfficher();
+
+
+        function updateDonneAfficher() {
+            const deleteButtons = document.querySelectorAll(".Supprimer");
+            const editButtons = document.querySelectorAll(".Mofifier");
+
+            deleteButtons.forEach((button) =>
+                button.addEventListener('click',() => deleteTerrain(button.id))
+            );
+            editButtons.forEach((button) =>
+                button.addEventListener('click',() => modifiers(button.id))
+            );
+        }
+
+
+        function ajoutePlacePrix(e) {
+            e.preventDefault();
+
+            const entreDonnee = {
+                id: terrains.length !== 0 ? terrains[terrains.length-1].id + 1 : 1,
+                place : document.getElementById('place').value,
+                prix: document.getElementById('prix').value,
+            };
+            if (entreDonnee.place !== "" && entreDonnee.prix !== "") {
+                terrains.push(entreDonnee);
+                afficheTout();
+            }
+
+        }
+
+        function afficheTout() {
+            document.getElementById('allterrains').innerHTML = "";
+            terrains.forEach((dim) =>{
+                const newInput = {
+                    Place : document.createElement('input'),
+                    Prix : document.createElement('input'),
+                };
+
+                const newDiv = document.createElement('div');
+                const newButtons = {
+                    Supprimer: document.createElement('input'),
+                    Modifier : document.createElement('input'),
+                };
+
+                for(const [key, value] of Object.entries(newInput)){
+                    value.setAttribute("type", "text");
+                    value.setAttribute("id", `${key}OfTerrain${dim.id}`);
+
+                    key === "Place" && value.setAttribute("value", dim.place);
+                    key === "Prix" && value.setAttribute("value", dim.prix);
+
+                    newDiv.appendChild(value);
+                    document.getElementById('allterrains').appendChild(newDiv);
+                };
+
+                for(const [key, value] of Object.entries(newButtons)){
+                    value.setAttribute('type', 'button');
+                    value.setAttribute('class', key);
+                    value.setAttribute('id', dim.id);
+                    value.setAttribute('value', key);
+
+                    newDiv.appendChild(value);
+
+                }
+            });
+
+            updateDonneAfficher();
+        }
+
+
+        function deleteTerrain(id) {
+            terrains.forEach((dim) => {
+                const userPositionInArray = terrains.indexOf(dim);
+                dim.id === parseInt(id) && terrains.splice(userPositionInArray, 1);
+            });
+
+            afficheTout();
+        }
+
+        function modifiers(id) {
+            const newInput = {
+                place : document.getElementById(`PlaceOfTerrain${id}`).value,
+                prix : document.getElementById(`PrixOfTerrain${id}`).value,
+            };
+
+            terrains.forEach((dim) => {
+                if (dim.id === parseInt(id)) {
+                    dim.place = newInput.place;
+                    dim.prix = newInput.prix;
+                }
+            });
         }
     </script>
 </x-app-layout>
