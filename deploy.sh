70507c731ce02b62d2820e1cf8c6242d9094c648
tchabana <hafiztchabana2004@gmail.com>
2024-11-26 17:00:45 +0000

    npm install

--- deploy.sh
+++ deploy.sh
@@ -74,7 +74,7 @@ php artisan migrate --force
 echo "Configuration des permissions..."
 chmod -R 775 storage bootstrap/cache
 chown -R www-data:www-data $APP_DIR
-sudo npm run install
+sudo npm install
 sudo npm run build
 # Redémarrer les services nécessaires
 echo "Redémarrage du serveur web..."
